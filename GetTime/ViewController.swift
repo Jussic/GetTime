a48177eab789f640d3e708ec97ff9a922e0db8ae
Jussic <justineclarkes@gmail.com>
2014-10-23 18:45:33 -0400

    Get Time
    Simple Timer built using SWIFT

--- GetTime/ViewController.swift
+++ GetTime/ViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timeCount = 0
+    var timeRunning = false
+    var timer = NSTimer()
 
+    @IBOutlet weak var timeLabel: UILabel!
+    func counting ()
+    {
+        timeCount += 1
+        timeLabel.text = "\(timeCount)"
+    }
+    
+    @IBAction func startButton(sender: UIButton) {
+        if timeRunning == false
+        { timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("Counting"), userInfo: nil, repeats: true)
+            timeRunning = true
+        }
+    }
+    
+ 
+    @IBAction func stopButton(sender: UIButton) {
+        
+        if timeRunning == true
+        {   timer.invalidate ()
+            timeRunning = false
+        }
+    }
+    
+
+    
+    @IBAction func restartButton(sender: UIButton) {
+        timeCount = 0
+        timeLabel.text = "0"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
